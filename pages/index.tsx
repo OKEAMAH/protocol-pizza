629132c331e80a6299c7e1795410cfef9afcc643
Jacob Peddicord <jpmail17@gmail.com>
2022-06-15 18:59:06 -0400

    few if any

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,17 +9,11 @@ SyntaxHighlighter.registerLanguage("typescript", typescript);
 export default function Landing() {
   return (
     <div className="bg-orange-50 h-full">
-      <div className="flex flex-row justify-end py-5 px-10">
-        <Link href="/order" passHref>
-          <button className="bg-orange-600 rounded px-3 py-1 text-lg text-white">
-            Launch App
-          </button>
-        </Link>
-      </div>
-      <div className="max-w-2xl mx-auto px-5 py-10 flex flex-col gap-10">
-        <div>
-          <p className="text-3xl">Protocol Pizza</p>
-          <p className="text-sm">
+      <div className="flex flex-row py-5 px-10 gap-2 items-center">
+        <div className="text-3xl">🍕</div>
+        <div className="flex flex-col">
+          <p className="text-2xl font-black leading-none">Protocol Pizza</p>
+          <p className="text-sm leading-none">
             Powered by{" "}
             <a
               href="https://rwtp.org"
@@ -29,6 +23,20 @@ export default function Landing() {
               RWTP
             </a>
           </p>
+        </div>
+        <div className="flex-grow"></div>
+        <Link href="/order" passHref>
+          <button className="bg-orange-600 rounded px-3 py-1 text-white">
+            Launch App
+          </button>
+        </Link>
+      </div>
+      <div className="max-w-2xl mx-auto px-5 py-10 flex flex-col gap-10">
+        <div>
+          <p className="text-3xl font-serif italic">
+            "Blockchain technologies facilitate few, if any, real-economy uses."
+          </p>
+          <p className="text">- Someone who never bought a pizza with crypto</p>
         </div>
       </div>
     </div>
